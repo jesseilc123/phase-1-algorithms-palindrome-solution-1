512271fdd337174de6fab22f8bc7e5445ec41b72
Jesse Ilc <jesseilc123@gmail.com>
2023-03-02 12:08:49 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+function reverse(word){
+  // 'abc' => 'cba'
+  // const wordArray = word.split("")
+  // const reversedWordArray = wordArray.reverse()
+  // const reversedWord = reversedWordArray.join("")
+  // return reversedWord;
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
 }
 
 /* 
+  In your own words
+Make a function that returns true if a word is a palindorme and false if not.
+that means if the word is the same as the word in reverse, I should return true.
+/* 
+/* 
   Add your pseudocode here
+that means if the word is the same as the word in reverse, I should return true.
+
+reverse the input string
+
+if the input is the same as the reversed input
+  return true
+else 
+  return false
 */
 
 /*
@@ -20,6 +44,31 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+  
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 }
 
 module.exports = isPalindrome;
